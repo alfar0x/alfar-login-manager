a91c5abcfddc2f9fee9ca25ae71502f0e58be9ea
John Doe <johndoe@example.com>
2024-02-26 12:41:42 +0200

    websites

--- src/content.js
+++ src/content.js
@@ -1,15 +1,26 @@
 import "./content.css";
 import discordPanel from "./discord-panel";
 
+const websites = [
+  {
+    hostnames: ["discord.com"],
+    func: discordPanel,
+  },
+];
+
 function inject(recursive = false) {
   const isInjectable = ["complete", "interactive"].includes(
     document.readyState
   );
 
   if (isInjectable) {
+    const func = websites.find((w) =>
+      w.hostnames.includes(location.hostname)
+    )?.func;
+    if (!func) return;
     const scriptEl = document.createElement("script");
     scriptEl.setAttribute("data-source", "token manager"),
-      (scriptEl.innerHTML = `(${discordPanel.toString()})();`);
+      (scriptEl.innerHTML = `(${func.toString()})();`);
     setTimeout(() => document.body.appendChild(scriptEl), 1000);
   } else {
     document.onreadystatechange = function () {
